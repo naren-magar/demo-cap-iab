849270347bbdcf4921a082d6b6dd80cc9876129e
iqnowworldpay <narendra.thapamagar@worldpay.com>
2020-06-10 13:55:36 -0400

    NOJIRA | Naren | Added push notification config in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Plugins, StatusBarStyle } from '@capacitor/core';
+import {
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private platform: Platform) {
     this.initializeApp();
+  }
+
+  ngOnInit(): void {
+    console.log('Initializing HomePage');
+
+    const { PushNotifications } = Plugins;
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then( result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+        (token: PushNotificationToken) => {
+          console.log('Push registration success, token: ' + token.value);
+        }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+        (error: any) => {
+          console.log('Error on registration: ' + JSON.stringify(error));
+        }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+        (notification: PushNotification) => {
+          console.log('Push received: ' + JSON.stringify(notification));
+        }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+        (notification: PushNotificationActionPerformed) => {
+          console.log('Push action performed: ' + JSON.stringify(notification));
+        }
+    );
   }
 
   async initializeApp() {
